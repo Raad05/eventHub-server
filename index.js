22f4a678ad3cb5c9f80c47571c620472c533bfc7
Raad05 <ashabulraad@gmail.com>
2024-02-13 04:51:49 +0600

    added client url

--- index.js
+++ index.js
@@ -9,23 +9,11 @@ const app = express();
 
 app.use(express.json());
 
-// Configure CORS
-const allowedOrigins = ["https://event-hub-pi.vercel.app"];
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
+    origin: ["http://localhost:3000", "https://event-hub-pi.vercel.app/"],
   })
 );
-
 config();
 
 app.post("/api/v1/url", async (req, res) => {
